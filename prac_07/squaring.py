4e2ef072bc1dcebdd095d4ebc42b31cdeced746b
cassidychen123 <75057769+cassidychen123@users.noreply.github.com>
2021-01-06 14:48:36 +0800

    Add files via upload

---
+++ prac_07/squaring.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class SquareNumberApp(App):
+    def build(self):
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        try:
+            result = int(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = ""
+
+
+SquareNumberApp().run()
